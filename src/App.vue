e1b89ca7d28a4d4274cfc9ddfab4557014223aac
ZeYn-Galaxy <shafwan066@gmail.com>
2023-07-25 23:57:41 +0800

    Kelompok

--- src/App.vue
+++ src/App.vue
@@ -29,14 +29,34 @@ const Generate = () => {
         next = next >= data.kelompok - 1 ? 0 : next + 1
       }
     }
-    GenerateByContinue()
+    let GenerateByRandomKelompok = () => {
+      let sudah = []
+      for (let i = 1; i <= anggota.length; i++) {
+        let TambahAnggota = () => {
+          let RandomAnggota = () => {
+            return Math.ceil(Math.random() * anggota.length)
+          }
+          let Anggota = RandomAnggota()
+          if (sudah.includes(Anggota)) {
+            TambahAnggota()
+          } else {
+            sudah.push(Anggota)
+            kelompok[next].push(Anggota)
+            next = next >= data.kelompok - 1 ? 0 : next + 1
+          }
+        }
+        TambahAnggota()
+      }
+    }
+
+    GenerateByRandomKelompok()
     // Input ke  reactiveKelompok
     for (const iterator of kelompok) {
       DataGenerate.Kelompok.push(iterator)
     }
-    console.log(DataGenerate.Kelompok);
   }
 }
+
 
 
 </script>
@@ -65,7 +85,8 @@ const Generate = () => {
   <!-- LIST -->
   <div class="bg-slate-100 w-full h-4/5 m-4 flex flex-wrap justify-center overflow-y-scroll rounded">
     <!-- CARD -->
-    <div class="bg-slate-100 w-80 h-1/2 flex m-2 flex-col overflow-y-scroll rounded shadow-md" v-for="(item, index) in DataGenerate.Kelompok" :key="index">
+    <div class="bg-slate-100 w-80 h-1/2 flex m-2 flex-col overflow-y-scroll rounded shadow-md"
+      v-for="(item, index) in DataGenerate.Kelompok" :key="index">
       <header class="flex justify-start w-full bg-slate-200 px-2 shadow-md z-20">
         <span class="text-lg w-1/5 text-center font-bold ">ABSEN</span>
         <span class="text-lg w-4/5 px-14 font-bold">KELOMPOK {{ index + 1 }}</span>
@@ -73,7 +94,8 @@ const Generate = () => {
       <!-- LIST -->
       <div v-for="(id, no) in item" class="w-full flex flex-wrap shadow-md z-10" :class="[no % 2 == 0 ? 'z-0' : 'z-10']">
         <span class="h-7 w-1/5 text-center" :class="[no % 2 == 0 ? 'bg-slate-200' : 'bg-slate-300']">{{ id }}</span>
-        <input class="h-7 w-4/5 text-center outline-none truncate px-2" :class="[no % 2 == 0 ? 'bg-slate-100' : 'bg-slate-200']" placeholder="Nama Anggota...">
+        <input class="h-7 w-4/5 text-center outline-none truncate px-2"
+          :class="[no % 2 == 0 ? 'bg-slate-100' : 'bg-slate-200']" placeholder="Nama Anggota...">
       </div>
     </div>
   </div>
